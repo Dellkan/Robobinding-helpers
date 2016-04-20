519465f396433c2551ebfe014bfb30804ca99af6
John <dellkan@hotmail.com>
2016-04-20 16:12:22 +0200

    WebView now supports "runJavascript" attribute, which does exactly what it sounds like.

--- robobinding-helpers-common/src/main/java/com/dellkan/robobinding/helpers/common/viewattributes/WebView/WebViewAttributes.java
+++ robobinding-helpers-common/src/main/java/com/dellkan/robobinding/helpers/common/viewattributes/WebView/WebViewAttributes.java
@@ -1,7 +1,11 @@
 package com.dellkan.robobinding.helpers.common.viewattributes.WebView;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.util.Log;
 import android.view.View;
+import android.webkit.ConsoleMessage;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -12,13 +16,14 @@ import org.robobinding.attribute.ChildAttributeResolverMappings;
 import org.robobinding.attribute.ResolvedGroupAttributes;
 import org.robobinding.attribute.StaticResourceAttribute;
 import org.robobinding.attribute.ValueModelAttribute;
-import org.robobinding.viewattribute.grouped.ChildViewAttributeFactory;
 import org.robobinding.viewattribute.grouped.ChildViewAttributeWithAttribute;
 import org.robobinding.viewattribute.grouped.ChildViewAttributesBuilder;
 import org.robobinding.viewattribute.grouped.GroupedViewAttribute;
 import org.robobinding.viewattribute.property.OneWayPropertyViewAttribute;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.robobinding.attribute.ChildAttributeResolvers.propertyAttributeResolver;
@@ -27,6 +32,8 @@ import static org.robobinding.attribute.ChildAttributeResolvers.staticResourceAt
 public class WebViewAttributes implements GroupedViewAttribute<WebView> {
     private WebViewErrorLayoutAttribute errorLayoutAttribute;
     private WebViewAdditionalHeadersAttribute mHeadersAttribute;
+    private WebViewRunJavascriptAttribute mRunJavascriptAttribute;
+    private boolean mFinishedLoading = false;
     @Override
     public String[] getCompulsoryAttributes() {
         return new String[] {"src"};
@@ -36,6 +43,7 @@ public class WebViewAttributes implements GroupedViewAttribute<WebView> {
     public void mapChildAttributeResolvers(ChildAttributeResolverMappings resolverMappings) {
         resolverMappings.map(propertyAttributeResolver(), "additionalHeaders");
         resolverMappings.map(propertyAttributeResolver(), "src");
+        resolverMappings.map(propertyAttributeResolver(), "runJavascript");
         resolverMappings.map(staticResourceAttributeResolver(), "errorLayout");
     }
 
@@ -52,17 +60,45 @@ public class WebViewAttributes implements GroupedViewAttribute<WebView> {
             ));
             mHeadersAttribute.setInitialHeaders(bindingContext);
         }
+        if (childViewAttributesBuilder.hasAttribute("runJavascript")) {
+            childViewAttributesBuilder.add("runJavascript", mRunJavascriptAttribute = new WebViewRunJavascriptAttribute());
+        }
         childViewAttributesBuilder.add("src", new WebViewSourceAttribute());
         if (childViewAttributesBuilder.hasAttribute("errorLayout")) {
             childViewAttributesBuilder.add("errorLayout", errorLayoutAttribute = new WebViewErrorLayoutAttribute(view.getRootView()));
         }
     }
 
+    private List<String> runJavascriptQueue = new ArrayList<>();
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     public void postBind(WebView webView, BindingContext bindingContext) {
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("WebView", consoleMessage.toString());
+                return super.onConsoleMessage(consoleMessage);
+            }
+        });
         webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                mFinishedLoading = false;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                mFinishedLoading = true;
+                if (mRunJavascriptAttribute != null) {
+                    for (String script : runJavascriptQueue) {
+                        mRunJavascriptAttribute.updateView(view, script);
+                    }
+                }
+                runJavascriptQueue.clear();
+            }
+
             public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
                 if (errorLayoutAttribute != null) {
                     view.setVisibility(View.GONE);
@@ -80,6 +116,7 @@ public class WebViewAttributes implements GroupedViewAttribute<WebView> {
         settings.setJavaScriptEnabled(true);
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
     }
 
     // Attribute for error
@@ -144,6 +181,22 @@ public class WebViewAttributes implements GroupedViewAttribute<WebView> {
             return convertedHeaders;
         }
     }
+
+    private class WebViewRunJavascriptAttribute implements OneWayPropertyViewAttribute<WebView, String> {
+        @Override
+        public void updateView(WebView view, String newValue) {
+            if (mFinishedLoading) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    view.evaluateJavascript(newValue, null);
+                } else {
+                    view.loadUrl(String.format("javascript:%s", newValue));
+                }
+            } else {
+                runJavascriptQueue.add(newValue);
+            }
+        }
+    }
+
     // Attribute for source
     private class WebViewSourceAttribute implements OneWayPropertyViewAttribute<WebView, String> {
         @Override
